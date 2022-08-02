b79ffb9236d8baf67eac4ad25e20535d7d92384b
kanitmann <kanitmann90@gmail.com>
2022-08-02 23:03:21 +0530

    App.js created and chakraUI initialized

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Fonts from '../components/fonts'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div style={styles.screenbg}>
+      <ChakraProvider>
+        <Fonts />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </div>
+  )
+}
+
+const styles = {
+  screenbg: {
+    backgroundColor: '#262626',
+    flex: 1,
+  }
 }
 
 export default MyApp
