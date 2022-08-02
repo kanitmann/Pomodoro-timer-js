1dd7758bfdd4c8fd2eb9fcd159a7b927ea080399
kanitmann <kanitmann90@gmail.com>
2022-08-02 23:02:54 +0530

    Subheading bar added

---
+++ components/subheading.js
@@ -0,0 +1,48 @@
+import { InfoIcon, SettingsIcon } from "@chakra-ui/icons"
+import { Box, Heading, IconButton, Stack } from "@chakra-ui/react"
+
+const SubHeading = () => {
+    return (
+        <Heading as="h2" size="md">
+
+            <Stack
+                alignItems="center"
+                width="100%"
+                justifyContent="space-between"
+                spacing={8}
+                direction='row'
+                style={styles.subHeading}
+            >
+
+                <Box>
+                    <IconButton
+                        aria-label="Setting-Icon"
+                        icon={<SettingsIcon />}
+                        size="sm"
+                    />
+                </Box>
+
+                <Box fontFamily='montserrat'>
+                    Set focus time
+                </Box>
+
+                <Box>
+                    <IconButton
+                        aria-label="Info-Icon"
+                        icon={<InfoIcon />}
+                        size="sm"
+                    />
+                </Box>
+            </Stack>
+        </Heading>
+    )
+}
+
+const styles = {
+    subHeading: {
+        color: "gray",
+        paddingTop: 25,
+    }
+}
+
+export default SubHeading;
