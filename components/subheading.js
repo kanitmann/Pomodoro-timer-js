6e4e643c98a3b749cf17d214eb7b562372b8e1e9
kanitmann <kanitmann90@gmail.com>
2022-08-04 00:05:13 +0530

    Icons made clickable

--- components/subheading.js
+++ components/subheading.js
@@ -1,5 +1,6 @@
-import { InfoIcon, SettingsIcon } from "@chakra-ui/icons"
-import { Box, Heading, IconButton, Stack } from "@chakra-ui/react"
+import { InfoIcon } from "@chakra-ui/icons"
+import { IoLogoGithub } from 'react-icons/io5'
+import { Box, Heading, IconButton, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Stack } from "@chakra-ui/react"
 
 const SubHeading = () => {
     return (
@@ -15,25 +16,44 @@ const SubHeading = () => {
 
                 <Box>
                     <IconButton
+                        isRound={true}
                         aria-label="Setting-Icon"
-                        icon={<SettingsIcon />}
+                        icon={<IoLogoGithub />}
                         size="sm"
+                        onClick={() => { window.open('https://github.com/kanitmann/Pomodoro-timer-js') }}
                     />
                 </Box>
-
-                <Box fontFamily='montserrat'>
-                    Set focus time
-                </Box>
-
+                <Input
+                    placeholder="Coding Timer"
+                    fontSize='2vh'
+                    border="none"
+                    focusBorderColor="transparent"
+                    fontWeight='medium'
+                    textAlign="center"
+                ></Input>
                 <Box>
-                    <IconButton
-                        aria-label="Info-Icon"
-                        icon={<InfoIcon />}
-                        size="sm"
-                    />
+                    <Popover isLazy >
+                        <PopoverTrigger>
+                            <IconButton
+                                isRound={true}
+                                aria-label="Info-Icon"
+                                icon={<InfoIcon />}
+                                size="sm"
+                                onClick={() => { console.log("clicked") }}
+                            />
+                        </PopoverTrigger>
+                        <PopoverContent bg="#262626" color="#dadada" fontSize="0.85vw" w='40vh'>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverHeader fontFamily="montserrat" fontWeight='semibold'>What is Pomodoro?</PopoverHeader>
+                            <PopoverBody fontFamily="arial" fontWeight='normal'>Using this method, you break your workday into 25-minute chunks separated by five-minute breaks. These intervals are referred to as pomodoros.</PopoverBody>
+                        </PopoverContent>
+
+
+                    </Popover>
                 </Box>
             </Stack>
-        </Heading>
+        </Heading >
     )
 }
 
