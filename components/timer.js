b53bf4d1db989bc9d76e41a097bdeba60fe8473e
kanitmann <kanitmann90@gmail.com>
2022-08-04 19:44:22 +0530

    basic functionality completed

--- components/timer.js
+++ components/timer.js
@@ -1,14 +1,57 @@
 import { AddIcon, MinusIcon } from '@chakra-ui/icons'
 import { Box, Button, Center, Heading, IconButton, Stack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+
 
 const timer = () => {
+    const [time, setTime] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [sec, setSec] = useState(0);
+    function alertOption() {
+        setTime(0);
+        alert("Timer cannot be less than 00!");
+
+    }
+
+    useEffect(() => {
+        time < 0 ?
+            alertOption()
+            : {};
+        document.title = `${time} : ${seconds} - Pomodoro Timer`;
+
+    }, [sec])
+
+    const startTimer = () => {
+        var interval = setInterval(() => {
+            setSec(prev => prev + 1);
+        }
+            , 1000)
+    }
+    useEffect(() => {
+        if (sec != 0) {
+            console.log(time, seconds)
+            if (seconds == 0) {
+                setSeconds(59);
+                console.log(seconds);
+                setTime(time => time - 1);
+            }
+            else if (seconds == 0 && time == 0) {
+                alert("Timer is up!");
+                clearInterval(interval);
+            }
+            else {
+                setSeconds(seconds => seconds - 1);
+            }
+        }
+    }, [sec])
+
     return (
         <Heading style={styles.timerHeading}>
             <Stack
                 alignItems="center"
                 direction='row'
-                width="50%"
                 justifyContent="space-between"
                 spacing={12}
                 style={styles.timerContainer}
@@ -18,22 +61,69 @@ const timer = () => {
                         aria-label="Setting-Icon"
                         icon={<MinusIcon />}
                         size="md"
+                        onClick={() => { setTime(time - 5) }}
                     />
                 </Box>
-
-                <Box >
-                    12:00
+                <Box>
+                    {time < 10 ? '0' + time : time}
                 </Box>
+                <Box>
+                    :
+                </Box>
+                <Box>
+                    {seconds < 10 ? '0' + seconds : seconds}
+                </Box>
+
                 <Box>
                     <IconButton
                         aria-label="Setting-Icon"
                         icon={<AddIcon />}
                         size="md"
+                        onClick={() => { setTime(time + 5) }}
                     />
                 </Box>
             </Stack>
+            <Stack
+                alignItems="center"
+                direction={["column-reverse", null, "row"]}
+                justifyContent="space-between"
+                spacing={[4, null, 12]}
+                style={styles.timerContainer}
+            >
+                <Button size="md"
+                    colorScheme='blue'
+                    variant='outline'
+                    onClick={() => { setSeconds(1) }}
+                >
+                    1 min
+                </Button>
+                <Button
+                    size="md"
+                    colorScheme='yellow'
+                    variant='outline'
+                    onClick={() => { setTime(15) }}
+                >
+                    15 min
+                </Button>
+                <Button
+                    size="md"
+                    colorScheme='orange'
+                    variant='outline'
+                    onClick={() => { setTime(25) }}
+                >
+                    25 min
+                </Button>
+                <Button
+                    size="md"
+                    colorScheme='pink'
+                    variant='outline'
+                    onClick={() => { setTime(35) }}
+                >
+                    35 min
+                </Button>
+            </Stack>
             <Center style={styles.buttonStyle}>
-                <Button variantColor="teal" size="lg" >Start</Button>
+                <Button variantColor="teal" size="lg" onClick={startTimer}>Start</Button>
             </Center>
         </Heading>
     )
@@ -42,7 +132,6 @@ const timer = () => {
 const styles = {
     timerContainer: {
         paddingTop: 45,
-        display: "flex",
     },
     timerHeading: {
         FontFace: 'arial',
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Container maxW="container.sm" paddingTop="50px" >
       <Box marginTop={8}>
-        <Box boxShadow='xl' p='6' rounded='md'>
+        <Box width="100%" boxShadow='xl' p='6' rounded='md'>
           <Heading size='lg'>Pomodoro Timer 🍅</Heading>
           <Divider paddingTop="20px" />
           <SubHeading />
